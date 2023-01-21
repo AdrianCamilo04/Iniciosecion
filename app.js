47855460d7568f574c5c33f23b02e545775ef942
AdrianCamilo04 <76708781+AdrianCamilo04@users.noreply.github.com>
2023-01-21 12:54:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function captura() {
+    var correo = document.getElementById("correo").value;
+    var contraseña = document.getElementById("contraseña").value;
+    /*
+    if (correo == "") {
+        alert("ingresa correo")
+        document.getElementById("correo").focus();
+    }else {
+        if (contraseña == "") {
+            alert("ingresa la contraseña")
+            document.getElementById("contraseña").focus();
+    }
+    */
+    console.log(correo)
+    console.log(contraseña)
+}
